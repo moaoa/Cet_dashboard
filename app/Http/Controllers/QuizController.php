1097402e525109ae1ea6602352859087d4b03040
moaoa <moaadbn3@gmail.com>
2024-08-30 18:41:22 +0200

    feat: student quizzes

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use function Laravel\Prompts\table;
 
 class QuizController extends Controller
 {
@@ -45,6 +46,52 @@ class QuizController extends Controller
 
         $quiz = Quiz::create($request->all());
         return response()->json($quiz, 201);
+    }
+
+    public function answerQuiz(Request $request, String $quiz_id): JsonResponse
+    {
+        $student = User::query()->where('name', 'ahmad')->first();
+
+        $quiz = Quiz::findOrFail($quiz_id);
+
+        $user_answers = DB::table('user_answers')
+            ->join('questions', 'questions.id', '=', 'user_answers.question_id')
+            ->where('user_answers.user_id', $student->id)
+            ->where('questions.quiz_id', $quiz->id)->get();
+
+        $done = $user_answers->contains(function ($item) use ($student) {
+             return $item->user_id == $student->id;
+        });
+
+        if($done){
+           return response()->json(['message'=> 'عذرا تم اجتياز هذا الاختبار من قبل'], 422);
+        }
+
+        $validator = Validator::make($request->all(), [
+            '*' => 'array',
+            '*.question_id' => 'required|integer|min:1|exists:questions',
+            '*.answer' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $allQuestionsOptions = [];
+
+        $questions = $quiz->questions()->forEach(function ($question) {
+            array_push(...json_encode($question->options));
+        });
+
+        $answers = $request->all();
+
+        if(in_array($answers)){
+
+        }
+
+        UserAnswer::create([]);
     }
 
     /**
@@ -94,23 +141,31 @@ class QuizController extends Controller
         $quiz->delete();
         return response()->json(null, 204);
     }
-    // public function studentQuizzes(): JsonResponse
     public function studentQuizzes()
+    // public function studentQuizzes(): JsonResponse
     {
         $student = User::query()->where('name', 'ahmad')->first();
 
-        $quizzes = $student->groups()->first()->quizzes()->with('user', 'questions')->get();
+        $items = DB::table('quiz_groups')
+            ->join('quizzes', 'quizzes.id', '=', 'quiz_groups.quiz_id')
+            ->join('groups', 'groups.id', '=', 'quiz_groups.group_id')
+            ->join('subjects', 'subjects.id', '=', 'quizzes.subject_id')
+            ->join('questions', 'questions.quiz_id', '=', 'quizzes.id')
+            ->select('subjects.name as subject_name', 'start_time', 'end_time', 'note', 'questions.options')
+            ->get();
+
+        $quizzes = $student->groups()->first()->quizzes()->get();
 
         $user_answers = DB::table('user_answers')
             ->join('questions', 'questions.id', '=', 'user_answers.question_id')
             ->where('user_answers.user_id', $student->id)
             ->whereIn('questions.quiz_id', $quizzes->pluck('id'))->get();
 
-        $data =  $quizzes->map(function ($quiz) use ($user_answers, $student) {
-            $done = $user_answers->contains(function ($item) use ($student) {
-                 return $item->user_id == $student->id;
-            });
+        $done = $user_answers->contains(function ($item) use ($student) {
+             return $item->user_id == $student->id;
+        });
 
+        $data =  $quizzes->map(function ($quiz) use ($done) {
              return [
                 'id' => $quiz->id,
                 'note' => $quiz->note,
