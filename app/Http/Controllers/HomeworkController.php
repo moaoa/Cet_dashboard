57f13ee5e5314bb84ab02d2f7109218bd56b8cea
moaoa <moaadbn3@gmail.com>
2024-09-02 01:24:33 +0200

    fix: upload homework

--- app/Http/Controllers/HomeworkController.php
+++ app/Http/Controllers/HomeworkController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Homework;
+use App\Models\HomeworkUserAnswer;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Storage;
@@ -39,17 +41,23 @@ class HomeworkController extends Controller
         return response()->json($homework, 201);
     }
 
-    // public function uploadHomeworkAnswer(Request $request): JsonResponse
-    public function uploadHomeworkAnswer(Request $request)
+    public function uploadHomeworkAnswer(String $homework_id, Request $request): JsonResponse
     {
+        $student = User::query()->where('name', 'ahmad')->first();
         // Validate the file inputs
         $request->validate([
             'attachments.*' => 'required|file|mimes:jpg,jpeg,png,pdf,doc,docx|max:2048', // Example validation rules
         ]);
 
+        $homework = Homework::find($homework_id);
+
+        if($homework == null) {
+            return response()->json(['message' => 'لا يوجد واجب بهذا المعرف'], 422);
+        }
+
         if(!$request->hasFile('attachments')) return response()->json(['message' => 'No files uploaded'], 400);
 
-        $uploadedFiles = [];
+        $attachments = [];
 
         foreach ($request->file('attachments') as $file) {
             // Get the original file name
@@ -65,15 +73,23 @@ class HomeworkController extends Controller
 
             // Add the path to the array of uploaded files
 
-            $uploadedFiles[] = asset(Storage::url($path));
+            $attachments[] = ['name' => $fileName, 'url' =>asset(Storage::url($path))];
+        }
+
+        if(count($attachments) > 0)
+        {
+          HomeworkUserAnswer::create([
+              'user_id' => $student->id,
+              'homework_id' => $homework_id,
+              'attachments' => json_encode()
+          ]);
         }
 
         // Return a response with the paths of the uploaded files
         return response()->json([
             'message' => 'Files uploaded successfully',
-            'file_paths' => $uploadedFiles
+            'file_paths' => $attachments
         ]);
-
     }
 
     /**
