95c937bb41ce5802872e8501dd4992e342804b95
moaoa <moaadbn3@gmail.com>
2024-09-04 23:06:50 +0200

    fix: image in user resoruce

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\UserType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,7 +21,7 @@ class UserResource extends JsonResource
             'ref_number' => $this->ref_number,
             'email' => $this->email,
             'phone_number' => $this->phone_number,
-            'image' => ''
+            'image' => $this->type == UserType::Student ? 'https://www.clipartmax.com/png/small/284-2845924_graduate-icon-student-avatar-icon.png' : 'https://st2.depositphotos.com/3557671/11164/v/950/depositphotos_111644880-stock-illustration-man-avatar-icon-of-vector.jpg'
         ];
     }
 }
