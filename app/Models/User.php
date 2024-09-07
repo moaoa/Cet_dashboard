67a7aad582ef3b2df1c886d85e535b49a4b85083
moaoa <moaadbn3@gmail.com>
2024-09-07 22:31:44 +0200

    refactor: pulse logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,7 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Subject::class, 'user_subjects')->withPivot('passed');
     }
+    public function isAdmin(){
+        return $this->type == UserType::Student;
+    }
 }
