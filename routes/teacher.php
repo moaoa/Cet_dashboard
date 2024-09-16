17f775a59a8c3893cb8567f67cf46b28dc0df1fb
moaoa <moaadbn3@gmail.com>
2024-09-16 22:33:18 +0200

    fix: route

--- routes/teacher.php
+++ routes/teacher.php
@@ -22,6 +22,7 @@ Route::middleware('auth:teacher')->group(function () {
 
      Route::get('/quizzes', [QuizController::class, 'index']);
      Route::get('/quiz-results/{quiz}', [QuizController::class, 'quizResults']);
+     Route::get('/quiz/{quiz}/users/{user}/result', [QuizController::class, 'getStudentResult']);
 
      // Route::prefix('teacher')->group(function () {
      //     Route::post('/quiz-assignment', AssignQuizToGroupController::class);
