a0248239b72eb287be6280bbce455c44b201b192
moaoa <moaadbn3@gmail.com>
2024-09-16 16:20:57 +0200

    fix: teacher outes

--- routes/teacher.php
+++ routes/teacher.php
@@ -2,6 +2,8 @@
 use App\Http\Controllers\TakeAttendanceController;
 use App\Http\Controllers\Teacher\AuthController;
 use App\Http\Controllers\Teacher\GroupsOfSubjectController;
+use App\Http\Controllers\Teacher\QuizController;
+use App\Http\Controllers\UsersAttendingQuizController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Teacher\LecturesController;
 use App\Http\Controllers\Teacher\SubjectsController;
@@ -10,13 +12,18 @@ use App\Http\Controllers\AvailableClassRoomsController;
 Route::middleware('auth:teacher')->group(function () {
      Route::get('/lectures', [LecturesController::class, 'index']);
      Route::post('/lectures', [LecturesController::class, 'store']);
+
      Route::get('/subjects', [SubjectsController::class, 'index']);
-     Route::post('/attendance/{lecture}', TakeAttendanceController::class);
      Route::get('/subjects/{subject}/groups', [GroupsOfSubjectController::class, 'index']);
+
+     Route::post('/attendance/{lecture}', TakeAttendanceController::class);
+
      Route::get('/available-classrooms', AvailableClassRoomsController::class);
 
+     Route::get('/quizzes', [QuizController::class, 'index']);
+     Route::get('/quiz-students/{quizId}', UsersAttendingQuizController::class);
+
      // Route::prefix('teacher')->group(function () {
-     //     Route::get('/quiz-students/{quizId}', UsersAttendingQuizController::class);
      //     Route::post('/quiz-assignment', AssignQuizToGroupController::class);
      //     Route::post('/homework-assignment', AssignHomeworkToGroupController::class);
      // });
