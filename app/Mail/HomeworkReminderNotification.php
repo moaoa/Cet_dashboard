d6852f09fb78f49c259885a20e556c314a6a3bdc
moaoa <moaadbn3@gmail.com>
2024-09-24 01:09:42 +0200

    fix: remove shit

--- app/Mail/HomeworkReminderNotification.php
+++ app/Mail/HomeworkReminderNotification.php
@@ -24,34 +24,4 @@ class HomeworkReminderNotification extends Mailable
         return $this->subject('تذكير بالواجب')
             ->view('emails.homework-reminder');
     }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Homework Reminder Notification',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
 }
