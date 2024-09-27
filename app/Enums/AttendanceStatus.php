76f879f18955c665aa82aee0d829bb4f49fd9ce2
moaoa <moaadbn3@gmail.com>
2024-09-27 22:30:51 +0200

    feat: attendance in dashboard

--- app/Enums/AttendanceStatus.php
+++ app/Enums/AttendanceStatus.php
@@ -2,9 +2,21 @@
 
 namespace App\Enums;
 
-enum AttendanceStatus: int
+use Filament\Support\Contracts\HasLabel;
+
+enum AttendanceStatus: int implements HasLabel
 {
     case Absent = 1;
     case Present = 2;
     case AbsentWithPermission = 3;
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Absent => 'غائب',
+            self::Present => 'موجود',
+            self::AbsentWithPermission => 'غائب مع إذن',
+            default => 'غير معروف',
+        };
+    }
 }
--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -2,18 +2,22 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\AttendanceStatus;
 use App\Filament\Resources\AttendanceResource\Pages;
 use App\Filament\Resources\AttendanceResource\RelationManagers;
 use App\Models\Attendance;
+use App\Models\Group;
+use App\Models\Subject;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\View;
+
 
 class AttendanceResource extends Resource
 {
@@ -25,7 +29,20 @@ class AttendanceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('note')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('date')
+                    ->required(),
+                Forms\Components\Select::make('lecture_id')
+                    ->relationship('lecture', 'id')
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
             ]);
     }
 
@@ -33,26 +50,62 @@ class AttendanceResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name'),
-                TextColumn::make('ref_number'),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->formatStateUsing(fn($state) => AttendanceStatus::from($state)->getLabel()),
+                Tables\Columns\TextColumn::make('note'),
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('lecture.subject.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('lecture.group.name')
+                    ->numeric()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('lecture.teacher.name')
+                    ->numeric(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Filter::make('created_at')
+                    ->form([
+                        Forms\Components\Select::make('subject_id')
+                            ->options(Subject::all()->pluck('name', 'id')),
+                        Forms\Components\Select::make('group_id')
+                            ->options(Group::all()->pluck('name', 'id'))
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->whereHas('lecture', function (Builder $query) use ($data) {
+                                if ($data['subject_id'] == null) return $query;
+                                $query->where('subject_id', $data['subject_id']);
+                            })
+                            ->whereHas('lecture', function (Builder $query) use ($data) {
+                                if ($data['group_id'] == null) return $query;
+                                $query->where('group_id', $data['group_id']);
+                            });
+                    })
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
-
-    // public function getTableContent(): ?View
-    // {
-    //     return view('attendance');
-    // }
 
     public static function getRelations(): array
     {
