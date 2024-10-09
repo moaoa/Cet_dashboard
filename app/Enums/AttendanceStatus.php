293b3d5c4da2cfd5e1277b226f9811ef722321b0
moaoa <moaadbn3@gmail.com>
2024-10-09 16:19:18 +0200

    fix: attendance enum

--- app/Enums/AttendanceStatus.php
+++ app/Enums/AttendanceStatus.php
@@ -6,8 +6,8 @@ use Filament\Support\Contracts\HasLabel;
 
 enum AttendanceStatus: int implements HasLabel
 {
-    case Absent = 1;
-    case Present = 2;
+    case Present = 1;
+    case Absent = 2;
     case AbsentWithPermission = 3;
 
     public function getLabel(): string
