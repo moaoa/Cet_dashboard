0993a165ff485e52930f5eb0a6458919daeec05e
moaoa <moaadbn3@gmail.com>
2024-10-10 02:08:00 +0200

    fix: shit

--- app/Http/Controllers/Teacher/GroupsOfSubjectController.php
+++ app/Http/Controllers/Teacher/GroupsOfSubjectController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers\Teacher;
 use App\Http\Controllers\Controller;
 use App\Models\Group;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GroupsOfSubjectController extends Controller
 {
     public function index(Request $request, String $subject_id)
     {
         $teacher = $request->user();
-        $groups = Group::where('teacher_id', $teacher->id)->get();
+
+        $groups = DB::table('group_subject')
+            ->join('groups', 'groups.id', '=', 'group_subject.group_id')
+            ->join('subjects', 'subjects.id', '=', 'group_subject.subject_id')
+            ->where('subjects.id', $subject_id)
+            ->where('groups.teacher_id', $teacher->id)
+            ->select('groups.id', 'groups.name')
+            ->get();
 
         return response()->json($groups);
     }
