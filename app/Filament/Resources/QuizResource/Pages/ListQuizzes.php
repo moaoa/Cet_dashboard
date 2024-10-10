ed258164de9403439b2440f68d5d1afd1f7a3bfb
moaoa <moaadbn3@gmail.com>
2024-10-10 10:08:50 +0200

    fix: remoe create action from qujzi admin

--- app/Filament/Resources/QuizResource/Pages/ListQuizzes.php
+++ app/Filament/Resources/QuizResource/Pages/ListQuizzes.php
@@ -13,7 +13,7 @@ class ListQuizzes extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
