3135fe0df25aae3da2727ef3333549b044d9d2bd
moaoa <moaadbn3@gmail.com>
2024-10-12 23:48:56 +0200

    fix: widgets

--- app/Providers/Filament/CetPanelProvider.php
+++ app/Providers/Filament/CetPanelProvider.php
@@ -41,7 +41,7 @@ class CetPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
                 StudentAbsenceReport::class
             ])
-            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([])
             ->middleware([
                 EncryptCookies::class,
