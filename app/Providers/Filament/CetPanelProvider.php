ae5ac187f56d4ef3fbdca0ea53e3fb4228fee0eb
moaoa <moaadbn3@gmail.com>
2024-10-12 23:51:39 +0200

    fix: vite error

--- app/Providers/Filament/CetPanelProvider.php
+++ app/Providers/Filament/CetPanelProvider.php
@@ -56,7 +56,7 @@ class CetPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->viteTheme('resources/css/filament/cet/theme.css');
+            ]);
+        // ->viteTheme('resources/css/filament/cet/theme.css');
     }
 }
