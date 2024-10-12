e5c697d90aa96e8844e55b63de89e643bf66855a
moaoa <moaadbn3@gmail.com>
2024-10-12 18:10:38 +0200

    fix: students absence report

--- app/Livewire/StudentsAbsenceReport.php
+++ app/Livewire/StudentsAbsenceReport.php
@@ -3,23 +3,31 @@
 namespace App\Livewire;
 
 use App\Enums\AttendanceStatus;
+use App\Enums\Major;
 use App\Models\Attendance;
 use App\Models\Subject;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use App\Exports\ExcelExport;
+use App\Models\Group;
+use App\Models\Semester;
 use Maatwebsite\Excel\Facades\Excel;
 
 
 class StudentsAbsenceReport extends Component
 {
+    public $selectedSubject;
+    public $selectedMajor;
+    public $selectedSemester;
+    public $selectedGroup;
+
     public $absenceData = [];
     public $subjects = [];
-    public $selectedSubject;
+    public $semesters = [];
+    public $groups = [];
 
     public function render()
     {
-        $this->subjects = Subject::all();
         return view('livewire.students-absence-report');
     }
 
@@ -29,8 +37,10 @@ class StudentsAbsenceReport extends Component
             ->join('users', 'users.id', '=', 'attendances.user_id')
             ->join('lectures', 'lectures.id', '=', 'attendances.lecture_id')
             ->join('subjects', 'subjects.id', '=', 'lectures.subject_id')
-            ->when($this->selectedSubject != null, function ($query) {
-                return $query->where('subjects.id', $this->selectedSubject);
+            ->join('group_user', 'group_user.user_id', '=', 'users.id')
+            ->where('subjects.id', $this->selectedSubject)
+            ->when($this->selectedGroup != null, function ($query) {
+                return $query->where('group_user.group_id', $this->selectedGroup);
             })
             ->select(
                 'users.name',
@@ -43,6 +53,7 @@ class StudentsAbsenceReport extends Component
 
         $this->absenceData = $absenceData;
     }
+
     public function exportToExcel()
     {
         if ($this->absenceData) {
@@ -53,6 +64,83 @@ class StudentsAbsenceReport extends Component
                 ),
                 'absence_report.xlsx'
             );
+        }
+    }
+
+    public function getMajorOptions()
+    {
+        $options = [];
+
+        collect(Major::cases())
+            ->each(function ($major) use (&$options) {
+                $options[] = ['title' => $major->getLabel(), 'value' => $major->value];
+            });
+
+        return $options;
+    }
+
+    public function getSemesterOptions()
+    {
+        $options = [];
+
+        $semesters = Semester::where('major', $this->selectedMajor)->get();
+
+        foreach ($semesters as $semester) {
+            $options[] = ['title' => $semester->name, 'value' => $semester->id];
+        }
+
+        return $options;
+    }
+
+    public function updatedSelectedMajor()
+    {
+        $this->selectedSemester = null;
+        $this->selectedSubject = null;
+        $this->selectedGroup = null;
+
+        $semesters = Semester::where('major', $this->selectedMajor)->get();
+
+        $options = [];
+
+        foreach ($semesters as $semester) {
+            $options[] = ['title' => $semester->name, 'value' => $semester->id];
+        }
+
+        $this->semesters = $options;
+    }
+
+    public function updatedSelectedSemester()
+    {
+        $this->selectedSubject = null;
+        $this->selectedGroup = null;
+
+        $subjects = Subject::whereHas('semester', function ($query) {
+            $query->where('id', $this->selectedSemester)
+                ->where('major', $this->selectedMajor);
+        })->get();
+
+        $options = [];
+
+        foreach ($subjects as $item) {
+            $options[] = ['title' => $item->name, 'value' => $item->id];
+        }
+
+        $this->subjects = $options;
+    }
+
+    public function updatedSelectedSubject()
+    {
+        $this->selectedGroup = null;
+
+        $groups = Group::whereHas('subjects', function ($query) {
+            $query->where('id', $this->selectedSubject)
+                ->where('semester_id', $this->selectedSemester);
+        })->get();
+
+        $options = [];
+
+        foreach ($groups as $item) {
+            $options[] = ['title' => $item->name, 'value' => $item->id];
         }
     }
 }
