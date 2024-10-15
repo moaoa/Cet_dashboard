82077b15284c02691f9f96a655a876774f4c5782
moaoa <moaadbn3@gmail.com>
2024-10-15 21:44:07 +0200

    fix: student subjects query

--- app/Http/Controllers/UserSubjectController.php
+++ app/Http/Controllers/UserSubjectController.php
@@ -16,10 +16,16 @@ class UserSubjectController extends Controller
         $student = $request->user();
 
         $subjects = DB::table('user_subjects')
-            ->join('subject_teacher', 'subject_teacher.subject_id', '=', 'user_subjects.subject_id')
-            ->join('group_user', 'group_user.user_id', '=', 'user_subjects.user_id')
+            ->join('group_user', function ($join) use ($student) {
+                $join->on('group_user.user_id', '=', 'user_subjects.user_id')
+                    ->where('group_user.user_id', '=', $student->id);
+            })
+            ->join('teacher_groups', function ($join) use ($student) {
+                $join->on('teacher_groups.group_id', '=', 'group_user.group_id')
+                    ->where('group_user.user_id', '=', $student->id);
+            })
             ->join('groups', 'groups.id', '=', 'group_user.group_id')
-            ->join('teachers', 'teachers.id', '=', 'subject_teacher.teacher_id')
+            ->join('teachers', 'teachers.id', '=', 'teacher_groups.teacher_id')
             ->join('subjects', 'subjects.id', '=', 'user_subjects.subject_id')
             ->where('user_subjects.user_id', $student->id)
             ->select('subjects.id', 'subjects.name', 'groups.name as group_name', 'teachers.name as teacher_name')
